f6f2dfdd361fb9b624a11dc8d4d3b4d9c98b442c
MatteoP95 <mlfplumari@gmail.com>
2024-05-05 11:57:06 +0200

    Major: spotify API fully implemented

--- mhw3.js
+++ mhw3.js
@@ -211,8 +211,6 @@ const client_secret_spotify = '';
 const client_endpoint_spotify =  'https://accounts.spotify.com/api/token' ;
 let token;
 
-const canzoni_endpoint = 'https://api.spotify.com/v1/search?type=album&q=';
-
 
 fetch(client_endpoint_spotify,{
    method: "post",
@@ -243,29 +241,83 @@ function onJsonSpotify(json){
     const library = document.querySelector('#contenuto_spotify');
     library.innerHTML = '';
 
-    const risultati= json.albums.items;
-    let num_risultati = risultati.length;
-
-    for(let i=0; i<num_risultati; i++){
-        const album_data= risultati[i];
-        const immagine= album_data.images[0].url;
-        const titolo = album_data.name;
 
 
-        const album = document.createElement('div');
-        album.classList.add('album');
+    if(json.albums){
+        const risultati= json.albums.items;
 
-        const img = document.createElement('img');
-        img.src=immagine;
-
-        const caption=document.createElement('span');
-        caption.textContent=titolo;
-
-        album.appendChild(img);
-        album.appendChild(caption);
-        library.appendChild(album);
+        let num_risultati = risultati.length;
+    
+        for(let i=0; i<num_risultati; i++){
+            const album_data= risultati[i];
+            const immagine= album_data.images[0].url;
+            const titolo = album_data.name;
+    
+    
+            const album = document.createElement('div');
+            album.classList.add('album');
+    
+            const img = document.createElement('img');
+            img.src=immagine;
+    
+            const caption=document.createElement('span');
+            caption.textContent=titolo;
+    
+            album.appendChild(img);
+            album.appendChild(caption);
+            library.appendChild(album);
+        }    
     }
+    else if(json.artists){
+        const risultati= json.artists.items;
 
+        let num_risultati = risultati.length;
+
+        for(let i=0; i<num_risultati; i++){
+            const artist_data= risultati[i];
+            const immagine= artist_data.images[0].url;
+            const titolo = artist_data.name;
+    
+    
+            const artist = document.createElement('div');
+            artist.classList.add('artista');
+    
+            const img = document.createElement('img');
+            img.src=immagine;
+    
+            const caption=document.createElement('span');
+            caption.textContent=titolo;
+    
+            artist.appendChild(img);
+            artist.appendChild(caption);
+            library.appendChild(artist);
+        }  
+    }
+    else if(json.tracks){
+        const risultati= json.tracks.items;
+
+        let num_risultati = risultati.length;
+
+        for(let i=0; i<num_risultati; i++){
+            const track_data= risultati[i];
+            const immagine= track_data.album.images[0].url;
+            const titolo = track_data.name;
+    
+    
+            const track = document.createElement('div');
+            track.classList.add('canzone');
+    
+            const img = document.createElement('img');
+            img.src=immagine;
+    
+            const caption=document.createElement('span');
+            caption.textContent=titolo;
+    
+            track.appendChild(img);
+            track.appendChild(caption);
+            library.appendChild(track);
+        }  
+    }
 }
 
 function onResponseSpotify(response){
@@ -276,11 +328,17 @@ function onResponseSpotify(response){
 
 function ricerca_spotify(event){
     event.preventDefault();
+    let spotify_endpoint = 'https://api.spotify.com/v1/search?type=';
+    let tipo = document.querySelector("#tipo_spotify").value;
+
+
+    spotify_endpoint = spotify_endpoint + tipo + '&q=';
+
 
     const input = document.querySelector("#input_spotify");
     const value = encodeURIComponent(input.value);
     
-    fetch(canzoni_endpoint + value,{
+    fetch(spotify_endpoint + value,{
             headers:{
                 'Authorization': 'Bearer '+ token
             }
